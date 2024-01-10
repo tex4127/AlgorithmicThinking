d37eea61185864d89b6645508c4f2f451a6ecf83
tex4127 <95382726+tex4127@users.noreply.github.com>
2024-01-10 12:19:26 -0500

    Just some updates

---
+++ Chapter 1 - HashTables/Snowflakes/main.c
@@ -0,0 +1,20 @@
+/////////////////////////////////////////
+///
+//  This source file is the solution for the
+//  "Snowflakes" problem that starts on page 2.
+//  This is also on dmoj as problem cco07p2
+///
+/////////////////////////////////////////
+
+#include <stdlib.h>
+
+int main()
+{
+    //We need to first take in the number of snowflakes that will be given as input
+    int n, i, j;
+    scanf("%d", &n);
+
+    printf("Value entered was %d", n);
+    
+    return 0;
+}
